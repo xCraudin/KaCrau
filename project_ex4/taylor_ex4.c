7eeea6cb46726c24c0f83912fae517ae31d1fc43
Claudionor Domingues <64593472+xCraudin@users.noreply.github.com>
2022-05-01 21:21:31 +0000

    exercício de sequência de Taylor

---
+++ project_ex4/taylor_ex4.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+// Considere o algoritmo proposto para o projeto final da disciplina, que envolve a soma infinitesimal para solução de ln(T) em séries de Taylor
+
+int main(void) {
+  int T = 10;
+  float sum = 0;
+	
+  for (float i = 1; i <= T; i++)
+	{
+    sum += (1/i);
+  }
+
+	printf("A solução da Série de Taylor para Ln(10)\n");
+  printf("Ln(%d) = %.3f\n", T, sum);
+  return 0;
+}
